b79d727d965dffdc4bd105ca0405453cb0d8b73b
Ben Walker <benn.walker@gmail.com>
2015-07-01 17:16:03 +1000

    Finalised part 1 of guide

---
+++ README.md
@@ -0,0 +1,3 @@
+##Let's Build: Instagram - Part One
+
+The source code for the guide found [over here at devwalks!](http://www.devwalks.com/lets-build-instagram-in-rails-part-1/)
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def alert_for(flash_type)
+      {
+          :success => 'alert-success',
+          :error => 'alert-danger',
+          :alert => 'alert-warning',
+          :notice => 'alert-info'
+      }[flash_type.to_sym] || flash_type.to_s
+  end
 end

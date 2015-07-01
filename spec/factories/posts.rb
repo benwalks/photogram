040d56a71032bd0d7351644173cfc2b21fdf0969
Ben Walker <benn.walker@gmail.com>
2015-07-01 17:36:19 +1000

    Removed redundant test code

--- spec/factories/posts.rb
+++
@@ -1,6 +0,0 @@
-FactoryGirl.define do
-  factory :post do
-    caption "MyString"
-  end
-
-end
--- spec/features/posts_index_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-describe "Post Index" do
-  it "can view the index page" do
-    visit '/'
-
-    expect(page).to have_content("Photogram")
-  end
-end

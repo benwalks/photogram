8927fe7d919b03b122fc300364ecedbf91a4da3b
Ben Walker <benn.walker@gmail.com>
2015-07-13 11:37:39 +1000

    Added nested comments to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations' }
-  
+
   root 'posts#index'
 
-  resources :posts
+  resources :posts do
+    resources :comments
+  end
 
 end

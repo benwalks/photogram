674a3f2f085485f9ba66ccfadddc7aaeaeb4b648
Ben Walker <benn.walker@gmail.com>
2015-07-19 09:44:46 +1000

    Ignored local images in gitignore

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -20,7 +20,7 @@ class CommentsController < ApplicationController
     @comment = @post.comments.find(params[:id])
 
     if @comment.user_id == current_user.id
-      @comment.delete
+      @comment.destroy
       respond_to do |format|
         format.html { redirect_to root_path }
         format.js

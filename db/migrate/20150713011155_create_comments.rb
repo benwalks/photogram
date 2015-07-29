b694496f69ca6d8baef625abdda8208844bef556
Ben Walker <benn.walker@gmail.com>
2015-07-29 21:03:53 +1000

    removed 'drop_table :comments' line from the create comments migration.  Will cause errors for users cloning the repo.  Was only originally included to fix a mistake on my dev machine

--- db/migrate/20150713011155_create_comments.rb
+++ db/migrate/20150713011155_create_comments.rb
@@ -1,6 +1,5 @@
 class CreateComments < ActiveRecord::Migration
   def change
-    drop_table :comments
     create_table :comments do |t|
       t.references :user, index: true
       t.references :post, index: true

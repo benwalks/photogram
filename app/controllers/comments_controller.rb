4f5cff351c801739157d6a1ff9a50795fabb3e73
Ben Walker <benn.walker@gmail.com>
2015-08-12 08:16:07 +1000

    Fixed ordering of comments in order to remove the #reverse_each method

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,7 @@ class CommentsController < ApplicationController
   before_action :set_post
 
   def index
-    @comments = @post.comments.order("created_at DESC")
+    @comments = @post.comments.order('created_at asc')
 
     respond_to do |format|
       format.html { render layout: !request.xhr? }

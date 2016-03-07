8eebf34106a1d108094b3d51a6b5eb9c15fd3898
jrothrock14 <jrothrock14@gmail.com>
2016-03-06 21:53:09 -0700

    Fixed Comments

--- app/assets/javascripts/loadMoreComments.js
+++ app/assets/javascripts/loadMoreComments.js
@@ -3,12 +3,13 @@ Append.open = false;
 function ClickableCommentsLink(){
   $('.more-comments').click( function() {
     $(this).on('ajax:success', function(event, data, status,xhr) {
+      event.preventDefault();
       var postId = $(this).data("post-id");
       $("#comments_" + postId).html(data);
       $("#comments-paginator-" + postId).html("<a id='more-comments' data-post-id=" + postId + "data-type='html' data-remote='true' href='/posts/" + postId + "/comments>show morecomments</a>");
       Append.open = true;
-      Append.comment = true; //Setting the value of Append.comment to true
-      Append.link = false;  //Setting the value of Append.link to false, as we don't want to append a link to the end
+      Append.comment = true; 
+      Append.link = false;  
     }); 
   });
 }
@@ -28,8 +29,8 @@ $( document ).ready(function() {
   	Append.id = this;
   	Append.post_id = $(this).data("post-id");
   	if (Append.comment_count == undefined){ Append.comment_count = $(this).data("value"); }
-  	if(Append.comment_count < 4){ Append.comment = true; Append.link = false; } //Setting the value of Append.comment to true
-  	else if(Append.comment_count == 4){ Append.comment = false; Append.link = true; } //Setting the value of Append.comment to false, and append.comment_link to true
-  	else if(Append.comment_count > 4){ Append.comment = false; Append.link = false;  } //Setting the value of append.comment_link to false
+  	if(Append.comment_count < 4){ Append.comment = true; Append.link = false; } 
+  	else if(Append.comment_count == 4){ Append.comment = false; Append.link = true; } 
+  	else if(Append.comment_count > 4){ Append.comment = false; Append.link = false;  } 
   })
 });
